15c944c66a0986d49a975b51053d627e94bc910e
TheresLundqvist <theres.lundqvist@outlook.com>
2022-10-24 15:30:56 +0000

    Set up main structure for javascript functions

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+/* jshint esversion: 11 */
+
+function changeQuestions() {
+
+}
+
+function questionNumber() {
+
+}
+
+function changeOptions() {
+
+}
+
+function nextQuestion() {
+
+}
+
+function correctAnswer() {
+
+}
+
+function wrongAnswer() {
+
+}
+
+function howToPlay() {
+
+}
+
+function highScores() {
+    
+}
