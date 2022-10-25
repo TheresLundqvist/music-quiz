87de6a96d7750d5b60e8e4a9e90d1d7d062d11a2
TheresLundqvist <theres.lundqvist@outlook.com>
2022-10-25 13:23:04 +0000

    Add function to get next question

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,7 +24,9 @@ function shuffleQuestions() {
     getNextQuestion();
 }
 
-function questionNumber() {
+function getNextQuestion() {
+    changeQuestions(shuffledQuestions[questionIndex]);
+}
 
 }
 
