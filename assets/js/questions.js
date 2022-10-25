04f3f3c877403fb5a8d7b9664ea75a97e81cc40f
TheresLundqvist <theres.lundqvist@outlook.com>
2022-10-25 13:29:18 +0000

    Split questions to own file

---
+++ assets/js/questions.js
@@ -0,0 +1,19 @@
+/* jshint esversion: 11 */
+const questions = [
+    {
+        question: "Who is the artist of the recent new album the Uncanny Valley?",
+        answers: [
+            { text: "a", correct: true },
+            { text: "b", correct: false },
+            { text: "c", correct: false }
+        ]
+    },
+    {
+        question: `According to their 1974 hit, what were Brownsville Station doing "In The Boys' Room"?`,
+        answers: [
+            { text: "a", correct: true },
+            { text: "b", correct: false },
+            { text: "c", correct: false }
+        ]
+    }
+];
