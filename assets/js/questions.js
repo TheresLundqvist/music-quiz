ea4a5b832586cc4b5f9b9ac30318554c70a50331
TheresLundqvist <theres.lundqvist@outlook.com>
2022-10-25 15:53:33 +0000

    Add questions and answers to array

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,19 +1,155 @@
 /* jshint esversion: 11 */
 const questions = [
     {
-        question: "Who is the artist of the recent new album the Uncanny Valley?",
+        question: "Who is the lead singer of the band Coldplay?",
         answers: [
-            { text: "a", correct: true },
-            { text: "b", correct: false },
-            { text: "c", correct: false }
+            { text: "Chris Wallace", correct: false },
+            { text: "Chris Martin", correct: true },
+            { text: "Chris Connelly", correct: false }
         ]
     },
     {
-        question: `According to their 1974 hit, what were Brownsville Station doing "In The Boys' Room"?`,
+        question: "Which rap group released the album 'Straight Outta Compton'?",
         answers: [
-            { text: "a", correct: true },
-            { text: "b", correct: false },
-            { text: "c", correct: false }
+            { text: "N.W.A", correct: true },
+            { text: "Run-D.M.C.", correct: false },
+            { text: "Wu-Tang Clan", correct: false }
+        ]
+    },
+    {
+        question: "Which of these is NOT the name of an album released by English singer-songwriter Adele?",
+        answers: [
+            { text: "25", correct: false },
+            { text: "12", correct: true },
+            { text: "19", correct: false }
+        ]
+    },
+    {
+        question: "Which band released songs suchs as 'Rio', 'Girls on Film', and 'The Reflex'?",
+        answers: [
+            { text: "Duran Duran", correct: true },
+            { text: "Depeche Mode", correct: false },
+            { text: "The Cure", correct: false }
+        ]
+    },
+    {
+        question: "Sting, the lead vocalist of The Police, primarily plays what instrument?",
+        answers: [
+            { text: "Drums", correct: false },
+            { text: "Bass Guitar", correct: true },
+            { text: "Guitar", correct: false }
+        ]
+    },
+    {
+        question: "Which 80s band is fronted by singer/guitarist Robert Smith?",
+        answers: [
+            { text: "The Cure", correct: true },
+            { text: "New Order", correct: false },
+            { text: "The Smiths", correct: false }
+        ]
+    },
+    {
+        question: "Which Disney character sings the song 'A Dream is a Wish Your Heart Makes'?",
+        answers: [
+            { text: "Snow White", correct: false },
+            { text: "Cinderella", correct: true },
+            { text: "Pocahontas", correct: false }
+        ]
+    },
+    {
+        question: "What is the frontman's name of the metal band Megadeth?",
+        answers: [
+            { text: "Dave Mustaine", correct: true },
+            { text: "James Hetfield", correct: false },
+            { text: "Vince Neil", correct: false }
+        ]
+    },
+    {
+        question: "Who is the lead singer of Pearl Jam?",
+        answers: [
+            { text: "Stone Gossard", correct: false },
+            { text: "Eddie Vedder", correct: true },
+            { text: "Ozzy Osbourne", correct: false }
+        ]
+    },
+    {
+        question: "Whose signature guitar technique is called the 'windmill'?",
+        answers: [
+            { text: "Pete Townshend", correct: true },
+            { text: "Eddie Van Halen", correct: false },
+            { text: "Jimi Hendrix", correct: false }
+        ]
+    },
+    {
+        question: "Which brass instrument has the lowest pitch in an orchestra?",
+        answers: [
+            { text: "Trombone", correct: false },
+            { text: "Tuba", correct: true },
+            { text: "Saxophone", correct: false }
+        ]
+    },
+    {
+        question: "From which country did the song 'Gangnam Style' originate from?",
+        answers: [
+            { text: "South Korea", correct: true },
+            { text: "China", correct: false },
+            { text: "Japan", correct: false }
+        ]
+    },
+    {
+        question: "Which Nirvana album had a naked baby on the cover?",
+        answers: [
+            { text: "Bleach", correct: false },
+            { text: "Nevermind", correct: true },
+            { text: "Incesticide", correct: false }
+        ]
+    },
+    {
+        question: "What was the best selling album of 2015?",
+        answers: [
+            { text: "Adele, 25", correct: true },
+            { text: "Taylor Swift, 1989", correct: false },
+            { text: "Justin Bieber, Purpose", correct: false }
+        ]
+    },
+    {
+        question: "Which of the following is an album by punk rock band Anti-Flag?",
+        answers: [
+            { text: "No Pads, No Helmets...Just Balls", correct: false },
+            { text: "For Blood And Empire", correct: true },
+            { text: "Infinity On High", correct: false }
+        ]
+    },
+    {
+        question: "What Led Zeppelin album contains 'Stairway to Heaven'?",
+        answers: [
+            { text: "Led Zeppelin IV", correct: true },
+            { text: "Physical Graffiti", correct: false },
+            { text: "Led Zeppelin III", correct: false }
+        ]
+    },
+    {
+        question: "When was Gangnam Style uploaded to YouTube?",
+        answers: [
+            { text: "2012", correct: true },
+            { text: "2018", correct: false },
+            { text: "2014", correct: false }
+        ]
+    },
+    {
+        question: "What is the name of the album released in 2014 by American band Maroon 5?",
+        answers: [
+            { text: "X", correct: false },
+            { text: "V", correct: true },
+            { text: "III", correct: false }
+        ]
+    },
+    {
+        question: "Who is the frontman of the band 30 Seconds to Mars?",
+        answers: [
+            { text: "Jared Leto", correct: true },
+            { text: "Mike Shinoda", correct: false },
+            { text: "Matthew Bellamy", correct: false }
         ]
     }
 ];
